977785d3013dd711c90570c04f7501fd6d18c6f5
cyrilkym <cyril.kym@youengineering.ch>
2024-11-01 23:05:32 +0100

    camera base

--- code/src/routes/camera.tsx
+++ code/src/routes/camera.tsx
@@ -8,6 +8,8 @@ export const Route = createFileRoute('/camera')({
 
 function CameraComponent() {
   const videoRef: MutableRefObject<null | HTMLVideoElement> = useRef(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
   const deviceSupported = () => {
     return (
       'mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices
@@ -41,9 +43,53 @@ function CameraComponent() {
     })
   }, [videoRef])
 
+  const takeScreenshot = async () => {
+    if (!videoRef.current || !canvasRef.current) return
+
+    const video = videoRef.current
+    const canvas = canvasRef.current
+    canvas.width = video.videoWidth
+    canvas.height = video.videoHeight
+
+    const context = canvas.getContext('2d')
+    context?.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+    const base64Image = canvas.toDataURL('image/png') // Get the image as base64
+    console.log('Base64 Image:', base64Image)
+
+    // canvas.toBlob((blob) => {
+    //   if (blob) {
+    //     const file = new File([blob], 'screenshot.png', { type: 'image/png' })
+    //     uploadFile(file)
+    //   }
+    // }, 'image/png')
+  }
+
+  const uploadFile = async (file: File) => {
+    const formData = new FormData()
+    formData.append('file', file)
+
+    try {
+      const response = await fetch('https://yourfileservice.com/upload', {
+        method: 'POST',
+        body: formData,
+      })
+
+      if (response.ok) {
+        console.log('File uploaded successfully')
+      } else {
+        console.error('Failed to upload file')
+      }
+    } catch (error) {
+      console.error('Error uploading file:', error)
+    }
+  }
+
   return (
-    <div>
+    <div className='w-max-[1440px] mx-auto'>
       <video ref={videoRef} className="bg-default w-[500px] h-[400px]"></video>
+      <canvas ref={canvasRef} className='w-[500px] h-[400px]'></canvas>
+      <button onClick={() => takeScreenshot()}>takeScreenshot</button>
     </div>
   )
 }
