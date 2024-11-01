4bf12fe68b172405735e01451e00dea07b372def
Leon <leon.hochwimmer@breitband.ch>
2024-11-01 22:30:20 +0100

    Init tailwind & shadcn

--- code/src/routes/index.tsx
+++ code/src/routes/index.tsx
@@ -1,14 +1,19 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import { useState } from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: HomeComponent,
-})
+});
 
 function HomeComponent() {
+  const [count, setCount] = useState<number>(0);
+
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <p>Count: {count}</p>
+      <Button onClick={() => setCount(count + 1)}>COOOUUUUNT</Button>
     </div>
-  )
+  );
 }
