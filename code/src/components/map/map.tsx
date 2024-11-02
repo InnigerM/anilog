b5c27f6e346bbd8813a33fab28ac60ec8de5fca3
nadiakramer <nadia.kramer@youengineering.ch>
2024-11-02 13:18:52 +0100

    map toggle

--- code/src/components/map/map.tsx
+++ code/src/components/map/map.tsx
@@ -1,53 +1,54 @@
-import { MapContainer, TileLayer } from "react-leaflet";
-import { CollectionMarker } from "@/components/map/collection-marker";
-import { AreaOfInterest, LocationMarker } from "@/components/map";
+import { MapContainer, TileLayer } from 'react-leaflet';
+import {
+    AreaOfInterest,
+    Collections,
+    LocationMarker,
+    MapToggle,
+} from '@/components/map';
+import React, { useState } from 'react';
 
 export function Map() {
-  return (
-    <div>
-      <MapContainer
-        className="h-[90vh] relative"
-        center={[47.5601333, 7.5879957]}
-        zoom={15}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-        />
-        <LocationMarker />
-        <CollectionMarker
-          lat={47.547679}
-          lng={7.59609}
-          name={"Gemeine Wegwarte"}
-          id={1}
-        />
-        <CollectionMarker
-          lat={47.546837}
-          lng={7.598924}
-          name={"Sonnenblume"}
-          id={1}
-        />
-        <CollectionMarker
-          lat={47.549101}
-          lng={7.591053}
-          name={"Seerose"}
-          id={1}
-        />
-        <CollectionMarker
-          lat={47.547116}
-          lng={7.597547}
-          name={"Cannabis sativa"}
-          id={1}
-        />
-        <CollectionMarker
-          lat={47.547801}
-          lng={7.59851}
-          name={"Fetthenne"}
-          id={1}
-        />
-        <AreaOfInterest position={[47.5483177, 7.5784675]} />
-      </MapContainer>
-    </div>
-  );
+    const [mapType, setMapType] = useState<'aoi' | 'collection'>('aoi');
+
+    const collections = [
+        {
+            id: 0,
+            name: 'Sonnenblume',
+            lat: 47.54676,
+            lng: 7.598758,
+        },
+        {
+            id: 1,
+            name: 'Fette Henne',
+            lat: 47.547878,
+            lng: 7.595827,
+        },
+    ];
+
+    return (
+        <div>
+            <MapContainer
+                className="h-[90vh] relative"
+                center={[47.5601333, 7.5879957]}
+                zoom={15}
+                scrollWheelZoom={false}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+                />
+                <LocationMarker />
+                {mapType === 'collection' ? (
+                    <Collections collections={collections} />
+                ) : null}
+                <div>
+                    <AreaOfInterest position={[47.5483177, 7.5784675]} />
+                </div>
+            </MapContainer>
+            <MapToggle
+                mapType={mapType}
+                setMapType={(value) => setMapType(value)}
+            />
+        </div>
+    );
 }
