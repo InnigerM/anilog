b43301f0f2c18e9efb686214bb1338868b93d88a
megiSW <marco.inniger@sitewerk.ch>
2024-11-02 14:51:26 +0100

    feat: add splash screen

--- code/src/components/ui/typography.tsx
+++ code/src/components/ui/typography.tsx
@@ -1,9 +1,21 @@
-import {PropsWithChildren} from "react";
+import clsx from 'clsx';
+import { PropsWithChildren } from 'react';
 
-export function H1({children}: PropsWithChildren) {
-    return <h1 className='text-2xl mb-2'>{children}</h1>
+type WithClassName = {
+    className?: string;
+};
+
+export function H1({ children }: PropsWithChildren) {
+    return <h1 className="text-2xl mb-2">{children}</h1>;
 }
 
-export function H2({children}: PropsWithChildren) {
-    return <h2 className='text-xl mb-2'>{children}</h2>
+export function H2({ children }: PropsWithChildren) {
+    return <h2 className="text-xl mb-2">{children}</h2>;
+}
+
+export function Body({
+    children,
+    className,
+}: PropsWithChildren & WithClassName) {
+    return <p className={clsx('', className)}>{children}</p>;
 }
--- code/tailwind.config.js
+++ code/tailwind.config.js
@@ -1,26 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-			'balloon-flower-purple': '#7149FF',
-			"honeysuckle-yellow": '#E1FF6B',
-			"marigold-yellow": '#FFE875',
-			"mint-green": '#93D9B7',
-			"blossom-pink": '#FFB0E7',
-			"hibiscus-orange": '#FF8B6B',
-			"black-pepper": '#222222',
-			"dusty-miller-gray": '#6B6969',
-			"cotton-light-gray": '#A9A9A9'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+    darkMode: ['class'],
+    content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+    theme: {
+        extend: {
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
+            },
+            colors: {
+                'balloon-flower-purple': '#7149FF',
+                'honeysuckle-yellow': '#E1FF6B',
+                'marigold-yellow': '#FFE875',
+                'mint-green': '#93D9B7',
+                'blossom-pink': '#FFB0E7',
+                'hibiscus-orange': '#FF8B6B',
+                'black-pepper': '#222222',
+                'dusty-miller-gray': '#6B6969',
+                'cotton-light-gray': '#A9A9A9',
+            },
+            fontFamily: {
+                sans: ['Schoolbell', 'cursive'],
+                chalkboard: 'Chalkboard',
+            },
+        },
+    },
+    plugins: [require('tailwindcss-animate')],
 };
