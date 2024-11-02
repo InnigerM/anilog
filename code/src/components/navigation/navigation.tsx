f789912ee38fe5107e6baf1f23519b919ee406a7
Leon <leon.hochwimmer@breitband.ch>
2024-11-02 11:45:18 +0100

    Add shutter button logic

--- code/src/components/navigation/navigation.tsx
+++ code/src/components/navigation/navigation.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
+import {
+    Link,
+    Outlet,
+    createRootRoute,
+    useRouterState,
+} from '@tanstack/react-router';
+import { Button } from '../ui/button';
 
 export default function Navigation() {
+    const router = useRouterState();
+
     return (
-        <nav className="p-2 flex gap-2 text-lg fixed bottom-0 w-full justify-around">
+        <nav className="p-2 flex gap-2 text-lg fixed bottom-0 w-full justify-evenly bg-gray-800 text-white z-50">
             <Link
                 to="/"
                 activeProps={{
@@ -12,14 +20,20 @@ export default function Navigation() {
             >
                 Map
             </Link>
-            <Link
-                to="/camera"
-                activeProps={{
-                    className: 'font-bold',
-                }}
-            >
-                Camera
-            </Link>{' '}
+            {router.location.pathname === '/camera' ? (
+                <Button onClick={() => console.log('picture taken!')}>
+                    Take Pic
+                </Button>
+            ) : (
+                <Link
+                    to="/camera"
+                    activeProps={{
+                        className: 'font-bold',
+                    }}
+                >
+                    Camera
+                </Link>
+            )}
             <Link
                 to="/collection"
                 activeProps={{
