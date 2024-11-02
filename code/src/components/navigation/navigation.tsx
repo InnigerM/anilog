6c469903b0bc44fc95d2ac92131f759815ad6418
Leon <leon.hochwimmer@breitband.ch>
2024-11-02 16:30:08 +0100

    Update nav icon size

--- code/src/components/navigation/navigation.tsx
+++ code/src/components/navigation/navigation.tsx
@@ -19,27 +19,27 @@ export default function Navigation({ variant }: NavigationProps) {
     return (
         <nav
             className={cn(
-                'px-5 pb-5 pt-10 flex gap-2 text-lg fixed bottom-0 w-full justify-around transition-colors duration-300 bg-gray-800 text-white z-[1000]',
+                'px-3 pb-3 pt-7 flex gap-2 text-lg fixed bottom-0 w-full justify-around transition-colors duration-300 bg-gray-800 text-white z-[1000]',
                 variant === 'MAP' && 'bg-blossom-pink',
                 variant === 'CAMERA' && 'bg-mint-green',
                 variant === 'COLLECTION' && 'bg-hibiscus-orange',
             )}
         >
             <Link to="/">
-                <i className="icon-map text-3xl" />
+                <i className="icon-map text-4xl" />
             </Link>
             {router.location.pathname === '/camera' ? (
                 <i
                     onClick={handleScreenshot}
-                    className="icon-shutter text-3xl cursor-pointer"
+                    className="icon-shutter text-4xl cursor-pointer"
                 />
             ) : (
                 <Link to="/camera">
-                    <i className="icon-camera text-3xl" />
+                    <i className="icon-camera text-4xl" />
                 </Link>
             )}
             <Link to="/collection" activeOptions={{ exact: true }}>
-                <i className="icon-plant text-3xl" />
+                <i className="icon-plant text-4xl" />
             </Link>
         </nav>
     );
