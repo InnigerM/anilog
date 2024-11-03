2fec6d7d45db88982a9f91fc0c392c4ff92a4af7
Yves Wehrli <yves.wehrli@bluewin.ch>
2024-11-03 10:13:07 +0100

    feat: link from collection to plantDetail

--- code/src/components/ui/framedImage.tsx
+++ code/src/components/ui/framedImage.tsx
@@ -1,20 +1,29 @@
-import { Link } from "@tanstack/react-router";
+import { Link, linkOptions, LinkProps } from '@tanstack/react-router';
 
 type FramedImageProps = {
-  imgUrl: string;
-  link: string;
+    imgUrl: string;
+    linkOptions?: Omit<LinkProps, 'to' | 'children'>;
 };
 
-export default function FramedImage({ imgUrl, link }: FramedImageProps) {
+export default function FramedImage({ imgUrl, linkOptions }: FramedImageProps) {
+    const framePath = './img/frame.svg';
 
-  const framePath = "./img/frame.svg";
-
-  return (<Link to={link} className="flex justify-center">
-    <div className="relative flex direction justify-center items-center w-36 h-44">
-      <div className="w-24 h-36">
-        <img className="size-full object-cover" src={imgUrl} alt="" />
-      </div>
-      <img className="absolute top-0 size-full -translate-x-px" src={framePath} alt="" />
-    </div>
-  </Link>);
+    return (
+        <Link {...linkOptions} className="flex justify-center">
+            <div className="relative flex direction justify-center items-center w-36 h-44">
+                <div className="w-24 h-36">
+                    <img
+                        className="size-full object-cover"
+                        src={imgUrl}
+                        alt=""
+                    />
+                </div>
+                <img
+                    className="absolute top-0 size-full -translate-x-px"
+                    src={framePath}
+                    alt=""
+                />
+            </div>
+        </Link>
+    );
 }
