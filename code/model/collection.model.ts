114e8f375f5467f469398fef0775f786ae3e2873
megiSW <marco.inniger@sitewerk.ch>
2024-11-03 10:25:37 +0100

    feat: add personalized scans onto map

--- code/model/collection.model.ts
+++
@@ -1,7 +0,0 @@
-export type Collection = {
-    id: number;
-    name: string;
-    lat: number;
-    lng: number;
-    category: 'flower' | 'tree' | 'plant';
-};
--- code/src/components/map/collection-marker.tsx
+++ code/src/components/map/collection-marker.tsx
@@ -2,27 +2,28 @@ import { Marker, Popup } from 'react-leaflet';
 import { Button } from '@/components/ui/button';
 import { H2 } from '@/components/ui/typography';
 import { icon } from 'leaflet';
-import {Collection} from "@/model/collection.model";
+import { Link } from '@tanstack/react-router';
+import { ScannedPlant } from '@/lib/api/scans';
 
-export function CollectionMarker({ lat, lng, name, id, category }: Collection) {
-    function goToObject() {
-        console.log(
-            'TODO: implement navigation to object detail with id ' + id,
-        );
-    }
+export function CollectionMarker({ location, plant }: ScannedPlant) {
+    if (!location) return null;
 
     const collectionIcon = icon({
-        iconUrl: '/' + category + '.svg',
-        iconSize: [40, 70],
+        iconUrl: '/' + plant.type + '.svg',
+        iconSize: [25, 50],
         iconAnchor: [15, 50],
         popupAnchor: [5, -40],
     });
 
-    return lat === null || lng === null ? null : (
-        <Marker position={[lat, lng]} icon={collectionIcon}>
+    return location?.lat === null || location?.lng === null ? null : (
+        <Marker position={[location.lat, location.lng]} icon={collectionIcon}>
             <Popup>
-                <H2>{name}</H2>
-                <Button onClick={goToObject}>View in collection</Button>
+                <H2>{plant.name_common}</H2>
+                <Button>
+                    <Link to="/plants/$plantId" params={{ plantId: plant.id }}>
+                        View in collection
+                    </Link>
+                </Button>
             </Popup>
         </Marker>
     );
