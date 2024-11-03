bf903185ca36c9b45b25f1fdc3a3e4f8db312b75
megiSW <marco.inniger@sitewerk.ch>
2024-11-03 11:10:59 +0100

    chore: remove unnecessary log statements

--- code/src/lib/api/user.ts
+++ code/src/lib/api/user.ts
@@ -13,13 +13,11 @@ export const getUserByEmail = (email: string) =>
     queryOptions({
         queryKey: ['user', email],
         queryFn: async () => {
-            const { data, error, count } = await supabase
+            const { data } = await supabase
                 .from('user')
                 .select('*')
                 .eq('email', email)
                 .single();
-
-            console.log(data, error, count);
 
             return data as UserResponse;
         },
